f146e7489ea48bcea67db91fd0614c43938ab333
sanjay jat <sanjayjatsanjay22@gmail.com>
2023-08-03 08:29:50 +0530

    add route action for different feature

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   ADDNEW_TODO,
   GETALL_TODO,
@@ -8,26 +9,33 @@ import {
   DELETE_TODO,
   TOGGLE_TAB,
 } from "./type";
+import { useEffect } from "react";
 
 const API_URL = "http://localhost:8000";
 
 export const addNewTodo = (data) => async (dispatch) => {
   try {
     const res = await axios.post(`${API_URL}/todos`, { data });
-    console.log(res.json());
+    // console.log(res.json());
     dispatch({ type: ADDNEW_TODO, payload: res.data });
+    toast.success(" New Task added ");
   } catch (error) {
     console.log("Error while calling addNewTodo API ", error.message);
+    toast.error("Error while adding new Task");
   }
 };
 
 export const getAllTodos = () => async (dispatch) => {
   try {
+    // Show loading toast
     const res = await axios.get(`${API_URL}/todos`);
+    console.log(res);
+    
 
     dispatch({ type: GETALL_TODO, payload: res.data });
   } catch (error) {
     console.log("Error while calling getAllTodos API ", error.message);
+    toast.error("Error while loading ");
   }
 };
 
@@ -46,8 +54,10 @@ export const updateTodo = (id, data) => async (dispatch) => {
     const res = await axios.put(`${API_URL}/todos/${id}`, { data });
 
     dispatch({ type: UPDATE_TODO, payload: res.data });
+    toast.success("Task Updated Succesfully");
   } catch (error) {
     console.log("Error while calling updateTodo API ", error.message);
+    toast.error("Error while Deleting ");
   }
 };
 
@@ -56,8 +66,10 @@ export const deleteTodo = (id) => async (dispatch) => {
     const res = await axios.delete(`${API_URL}/todos/${id}`);
 
     dispatch({ type: DELETE_TODO, payload: res.data });
+    toast.warn("Task Deleted !");
   } catch (error) {
     console.log("Error while calling deleteTodo API ", error.message);
+    toast.error("Error While Deleting");
   }
 };
 
